10b43690edd5d6fa0c05eb826ee607affb4fcad4
moje <twojemail@example.com>
2024-11-24 13:53:17 +0100

    Zrobione zadanie 1

--- zadanie1/zadanie1.js
+++ zadanie1/zadanie1.js
@@ -1,17 +1,17 @@
 const processText = (text, { uppercase = false, reverse = false, removeSpecialChars = false } = {}) =>{
    if(uppercase==true){
-      text.toUpperCase()
+      text= text.toUpperCase()
    }
    if(reverse == true){
-      text= [...text].reverse().join('')
+      text= [...text].reverse().join('');
    }
    if(removeSpecialChars == true){
-      text = text.replace(,"")
+      text = text.replace(/[^a-zA-Z0-9\s]/g, '');
    }
    return text
   } 
 
   const textDoZmiany = "Jakis tekst do zmiany!!!!"
 
-  const textZmieniony = processText(textDoZmiany,{uppercase:true, reverse:false,removeSpecialChars:true})
+  const textZmieniony = processText(textDoZmiany,{uppercase:false, reverse:false ,removeSpecialChars:true})
   console.log(textZmieniony)
